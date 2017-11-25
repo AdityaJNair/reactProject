24573eda55f24234561d6d982f8b74a66aa8a221
AdityaJNair <adijnair@gmail.com>
2017-11-25 22:35:53 +1300

    Added lodash and uikit to package, and a button, but doesnt style using uikit

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,8 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React test</h1>
         </header>
+        <button class="uk-button uk-button-default uk-button-small">Content</button>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
