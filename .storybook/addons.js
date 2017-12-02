f0c29e7565f83244852ea245ae0723f1ff816c3e
AdityaJNair <adijnair@gmail.com>
2017-12-02 13:06:03 +1300

    Added story book and fixed test case

---
+++ .storybook/addons.js
@@ -0,0 +1,6 @@
+import '@storybook/addon-actions/register';
+import '@storybook/addon-links/register';
+import '@storybook/addon-comments/register';
+import '@storybook/addon-info/register';
+import '@storybook/addon-knobs/register'
+import '@storybook/addon-options/register';
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './images/logo.svg';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import { Button } from 'react-bootstrap';
